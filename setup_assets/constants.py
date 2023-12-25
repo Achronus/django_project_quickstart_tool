11f1ff26f995f0515b3f16336454213d7b43db0c
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-12-25 17:33:24 +0000

    Updated JS libraries dynamic
    - Updated scripts in `_base.html` to local `js` folder
    - Added functionality to `setup.py` for downloading the files to `firstapp/js`
    - Moved `SETUP` constants to `constants.py`
    - Added JS library `urls` and `filenames` to `constants.py`

--- setup_assets/constants.py
+++ setup_assets/constants.py
@@ -17,6 +17,15 @@ SETUP_ROOT_ASSETS_ROOT_FOLDER = 'root'
 SETUP_ROOT_ASSETS_STATIC_FOLDER = 'static'
 SETUP_ROOT_ASSETS_TEMPLATE_FOLDER = 'templates'
 
+SETUP_FIRSTAPP_DIR = 'firstapp'
+
+# Setup assets filepaths
+SETUP_ROOT_DIR = os.path.dirname(os.path.join(os.getcwd(), SETUP_ROOT_ASSETS_NAME))
+SETUP_ASSETS_DIR = os.path.join(SETUP_ROOT_DIR, SETUP_ROOT_ASSETS_NAME)
+SETUP_ASSETS_ROOT_DIR = os.path.join(SETUP_ASSETS_DIR, SETUP_ROOT_ASSETS_ROOT_FOLDER)
+SETUP_ASSETS_STATIC_DIR = os.path.join(SETUP_ASSETS_DIR, SETUP_ROOT_ASSETS_STATIC_FOLDER)
+SETUP_ASSETS_TEMPLATE_DIR = os.path.join(SETUP_ASSETS_DIR, SETUP_ROOT_ASSETS_TEMPLATE_FOLDER)
+
 
 # Settings and url file paths associated to STARTPROJECT
 ROOT_SETTINGS_PATH = os.path.join(SETTINGS_DIR, "settings.py")
@@ -29,3 +38,13 @@ FIRSTAPP_VIEWS_PATH = os.path.join(FIRSTAPP_DIR, "views.py")
 
 ROOT_STATIC_FOLDER_URL = os.path.join(FIRSTAPP_DIR, 'static')
 ROOT_TEMPLATE_FOLDER_URL = os.path.join(FIRSTAPP_DIR, 'templates')
+
+
+# JS library urls
+FLOWBITE_FILENAME = 'flowbite.min.js'
+HTMX_FILENAME = 'htmx.min.js'
+ALPINE_FILENAME = 'alpine.min.js'
+
+FLOWBITE_URL = f'node_modules/flowbite/dist/{FLOWBITE_FILENAME}'
+HTMX_URL = f'https://unpkg.com/htmx.org/dist/{HTMX_FILENAME}'
+ALPINE_URL = f'node_modules/alpinejs/dist/cdn.min.js'
